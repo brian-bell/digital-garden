1a6203478c809caf8eff0d2f894156e575753226
Brian Bell <bellbm@gmail.com>
2021-04-12 11:12:59 -0400

    fix - use VISUAL instead of EDITOR for mocking the text editor cmd.

--- tests/integration.rs
+++ tests/integration.rs
@@ -23,6 +23,36 @@ fn test_write_help() -> Result<()> {
 
 #[test]
 fn test_write_with_title() {
+    let (mut cmd, temp_dir) = setup_command();
+
+    let assert = cmd
+        .arg("write")
+        .arg("-t")
+        .arg("atitle")
+        .write_stdin("N\n".as_bytes())
+        .assert();
+
+    assert.success();
+
+    temp_dir
+        .child("atitle.md")
+        .assert(predicate::path::exists());
+}
+
+#[test]
+fn test_write_with_written_title() {
+    let (mut cmd, temp_dir) = setup_command();
+
+    let assert = cmd.arg("write").write_stdin("N\n".as_bytes()).assert();
+
+    assert.success();
+
+    temp_dir
+        .child("testing.md")
+        .assert(predicate::path::exists());
+}
+
+fn setup_command() -> (Command, assert_fs::TempDir) {
     let temp_dir = assert_fs::TempDir::new().unwrap();
 
     let mut cmd = Command::cargo_bin("garden").unwrap();
@@ -30,24 +60,13 @@ fn test_write_with_title() {
         .expect("expect to be in a dir")
         .join("tests")
         .join("fake-editor.sh");
-    
+
     if !fake_editor_path.exists() {
-        panic!(
-            "fake editor shell script could not be found")
+        panic!("fake editor shell script could not be found")
     }
 
-    let assert = cmd
-        .env("EDITOR", fake_editor_path.into_os_string())
-        .env("GARDEN_PATH", temp_dir.path())
-        .arg("write")
-        .arg("-t")
-        .arg("atitle")
-        .write_stdin("N\n".as_bytes())
-        .assert();
-        
-    assert.success();
+    cmd.env("VISUAL", fake_editor_path.into_os_string())
+        .env("GARDEN_PATH", temp_dir.path());
 
-    temp_dir
-        .child("atitle.md")
-        .assert(predicate::path::exists());
+    (cmd, temp_dir)
 }
